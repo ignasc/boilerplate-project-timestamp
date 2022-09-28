a14deccd41df6119b18138bce049a9ed22573b65
ignasc <>
2022-09-28 14:12:18 +0300

    Copied from replit to my github
    Project completed and passed freecodecamp test

--- index.js
+++ index.js
@@ -18,13 +18,62 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// date variable aquisition 
+app.get("/api/:date", function (req, res) {
+  var unixRegex = new RegExp(/^\d+$/);//regular expression to test valid unix timestamp format
+  let currentDate = new Date();
+  let jsonDate = {"unix": null, "utc": null};
+  let isUnix = false;
+  let isDate = false;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  //check if Date.parse returns a number and not NaN, otherwise check if it is unix timestamp
+  if(!isNaN(Date.parse(req.params.date))){
+    isDate = true;
+  } else if(unixRegex.test(req.params.date)){
+    isUnix = true;
+  };
+
+  //Modify jsonData object based on isUnix and isDate flags
+  //if no flags set to true, return "invalid date"
+  if(!isUnix && !isDate){
+    res.json({ "error": "Invalid Date"});
+    return;
+  };
+  
+  if(isUnix){
+    jsonDate.unix = parseInt(req.params.date);
+    jsonDate.utc = unixToUTC(req.params.date);
+    res.json(jsonDate);
+    return;
+  } else if(isDate){
+    jsonDate.unix = Date.parse(req.params.date);
+    jsonDate.utc = dateToUTC(req.params.date);
+    res.json(jsonDate);
+    return;
+  };
+  
+  res.json({ "WARNING": "end of apt.get reached with no response with date object"});
 });
 
+// empty :date, a.k.a. /api/
+app.get("/api/", function (req, res) {
+  let unixTimestamp = Date.parse( new Date() );//also converted to a number, rather than string
+  let utcTime = new Date().toUTCString();
+  res.json({"unix": unixTimestamp, "utc": utcTime});
+});
 
+//function to convert valid date to UTC format
+function dateToUTC(date){
+  let dateObject = new Date(date);
+  return dateObject.toUTCString();
+};
+
+//function to convert unix timestamp to UTC format
+function unixToUTC(date){
+  let dateObject = new Date(parseInt(date));
+  return dateObject.toUTCString();
+  
+};
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
